d559a01378eabb68806663625390433b60f82c7d
Jiale <95jiale@gmail.com>
2022-03-19 21:10:25 +0800

    final changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const initialValue = { username: "", email: "", password: "" };
+  const [formValues, setFormValue] = useState(initialValue);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  //----------------------------------------------------------
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValue({ ...formValues, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+  //---------------------------------------------
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  //--------------------------------------------------
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Passsword must be more than 4 characters!";
+    } else if (values.password.length > 10) {
+      errors.password = "Passsword must be less than 10 characters!";
+    }
+
+    return errors;
+  };
   return (
     <div className="container">
-      <form>
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="ui message success">Signed in successfully</div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+      <form onSubmit={handleSubmit}>
         <h1>Login Form</h1>
         <div className="ui divider"></div>
         <div className="ui form">
           <div className="field">
             <label htmlFor="">Username</label>
-            <input type="text" name="username" placeholder="Username" />
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={handleChange}
+            />
           </div>
+          <p>{formErrors.username}</p>
           <div className="field">
             <label htmlFor="">Email</label>
-            <input type="email" name="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
           </div>
+          <p>{formErrors.email}</p>
           <div className="field">
             <label htmlFor="">Password</label>
-            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
           </div>
+          <p>{formErrors.password}</p>
           <button className="fluid ui buttont blue">Submit</button>
         </div>
       </form>
